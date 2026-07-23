61bc47807a2e5aa6e44ba267ffb19d459b8904c6
Jessica Fan <jessicafan2603@gmail.com>
2026-07-23 18:41:40 +1200

    deleted static.yml

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -5,7 +5,7 @@ import './NotFound.css';
 function NotFound() {
   return (
         <section className='not-found-container'>
-            <img src='/images/img-2.jpg' alt='404 error background' className='not-found-background'/>
+            <img src='%PUBLIC_URL%/images/img-2.jpg' alt='404 error background' className='not-found-background'/>
             <h1>404</h1>
             <p>Oops! Page Not Found</p>
             <Link to='/' className='return-home'>Return to Home</Link>
--- src/components/pages/Private.js
+++ src/components/pages/Private.js
@@ -5,7 +5,7 @@ import './NotFound.css';
 function Private() {
   return (
         <section className='not-found-container'>
-            <img src='/images/img-2.jpg' alt='404 error background' className='not-found-background'/>
+            <img src='%PUBLIC_URL%/images/img-2.jpg' alt='404 error background' className='not-found-background'/>
             <h1>Sorry!</h1>
             <p>This repository is private following academic integrity guidelines.</p>
             <Link to='/' className='return-home'>Return Home</Link>
